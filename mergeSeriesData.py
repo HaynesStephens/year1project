23f39f91ec46dbbaf04f470f60824bc4239a655d
HaynesStephens <hfstephens13@gmail.com>
2019-01-07 10:48:43 -0600

    merging csv files for series data

--- mergeSeriesData.py
+++ mergeSeriesData.py
@@ -2,16 +2,16 @@ import pandas as pd
 
 csv1 = '/project2/abbot/haynes/ROCKE3D_output/pc_proxcenb_aqua5L_TL_500yr/ts_data.csv'
 csv2 = '/project2/abbot/haynes/ROCKE3D_output/pc_proxcenb_aqua5L_TL_500yr_rs2/ts_data.csv'
-new_csv = '/project2/abbot/haynes/ROCKE3D_output/pc_proxcenb_aqua5L_TL_500yr/ts_data_tot.csv'
+new_csv = '/project2/abbot/haynes/ROCKE3D_output/pc_proxcenb_aqua5L_TL_500yr/ts_data_new.csv'
 
 def mergeData(csv1, csv2):
     df1 = pd.read_csv(csv1)
     df2 = pd.read_csv(csv2)
     result = df1.append(df2)
+    result = result.index
     return result
 
 def saveMergedData(result, new_csv):
-    result['decade'] = result.index
     result.to_csv(new_csv)
 
 result = mergeData(csv1, csv2)
