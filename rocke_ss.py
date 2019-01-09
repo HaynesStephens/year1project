24f660206d7f6de64b369b27ccd43b51aaac06e9
HaynesStephens <hfstephens13@gmail.com>
2019-01-09 11:11:38 -0600

    setting up new SLURM runs

--- rocke_ss.py
+++ rocke_ss.py
@@ -10,6 +10,7 @@ import os
 import subprocess
 from netCDF4 import Dataset
 import pandas as pd
+from glob import glob
 
 ## ***SPECIFY EXPERIMENT & ITS LOCATION ON MIDWAY***
 runid = 'pc_proxcenb_ssc5L_TL_11p'
@@ -111,4 +112,7 @@ df = pd.DataFrame({'decade': np.arange(total_decs), 'radiation': global_rad.resh
                    'ice_thickness': global_ice_thickness.reshape(total_decs)})
 df.to_csv('ts_data.csv')
 
-os.system('rm *aij*')
+## Delete all but the last 10 aij files to use in the matrix map plots
+aij_list = glob('*aij*') # Get a list of all the aij files made
+for aij_file in aij_list[:-10]: # Cycle through a list of all but the last 10 aij files
+    os.system('rm {0}'.format(aij_file)) # Delete the aij file
