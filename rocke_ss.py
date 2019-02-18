75d9e30c5362c355973e4c676ee1c530e233b727
HaynesStephens <hfstephens13@gmail.com>
2019-02-18 14:42:49 -0600

    check out runs to see if at EQ

--- rocke_ss.py
+++ rocke_ss.py
@@ -29,6 +29,7 @@ year_list = range(startyear, endyear, 10)
 
 os.chdir(rundirectory)  # Switch on over to the run directory.
 
+
 for y in year_list:
     beg_dec = str(y)
     end_dec = str(y + 9)
@@ -124,7 +125,9 @@ df = pd.DataFrame({'decade': np.arange(total_decs), 'radiation': global_rad.resh
                    'ice_thickness': global_ice_thickness.reshape(total_decs)})
 df.to_csv('ts_data.csv')
 
+'''
 ## Delete all but the last 10 aij files to use in the matrix map plots.
 aij_list = sorted(glob('*aij*')) # Get a list of all the aij files made.
 for aij_file in aij_list[:-10]: # Cycle through a list of all but the last 10 aij files.
     os.system('rm {0}'.format(aij_file)) # Delete the aij file.
+'''
