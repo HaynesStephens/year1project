1ba7e1be31cf685d083d0c2c9d4766f7ea57ff3d
HaynesStephens <hfstephens13@gmail.com>
2019-02-18 16:32:18 -0600

    check out runs to see if at EQ

--- rocke_ss.py
+++ rocke_ss.py
@@ -13,10 +13,10 @@ import pandas as pd
 from glob import glob
 
 ## ***SPECIFY EXPERIMENT & ITS LOCATION ON MIDWAY***
-runid = 'pc_proxcenb_ssc5L_TL_26p'
+runid = 'pc_proxcenb_ssc5L_TL_34p'
 rundirectory = '/project2/abbot/haynes/ROCKE3D_output/' + runid
 startyear = 1950
-endyear = 3649
+endyear = 2769
 
 
 ## ***DEFINE TIME INTERVAL***
