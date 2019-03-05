0e88f9aa8b742215b8692c7cbe7678ed7ddd5dd9
HaynesStephens <hfstephens13@gmail.com>
2019-03-05 10:41:43 -0600

    map plots

--- matrix_plot/matrixMaps.py
+++ matrix_plot/matrixMaps.py
@@ -74,7 +74,7 @@ def makeSubplot(data, var, ax, row_num, col_num, ylabel, parallels, meridians, t
     # m.drawcoastlines()
     # m.fillcontinents(color='coral',lake_color='aqua')
     # draw parallels and meridians.
-    m.drawparallels(parallels, labels=[1,0,0,0], ax = ax, fontsize=8)
+    m.drawparallels(parallels, labels=[1,0,0,0], ax = ax, rotation=30, fontsize=8)
     m.drawmeridians(meridians, labels=[0,0,0,1], ax = ax, rotation=30, fontsize=8)
 
     ny=data.shape[0]
