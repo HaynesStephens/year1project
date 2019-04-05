d74f62f93bfcbbecdda881487fdf43299de7cbf0
HaynesStephens <hfstephens13@gmail.com>
2019-04-05 15:53:57 -0500

    checking on runs

--- rocke_ss.py
+++ rocke_ss.py
@@ -13,10 +13,10 @@ import pandas as pd
 from glob import glob
 
 ## ***SPECIFY EXPERIMENT & ITS LOCATION ON MIDWAY***
-runid = 'pc_proxcenb_ssc5L_TL_34p'
+runid = 'pc_proxcenb_ssc5L_TL_39p'
 rundirectory = '/project2/abbot/haynes/ROCKE3D_output/' + runid
 startyear = 1950
-endyear = 2769
+endyear = 2649
 
 
 ## ***DEFINE TIME INTERVAL***
