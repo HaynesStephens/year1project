78606ab58de344a13672b7a2a343f437734bcb3c
HaynesStephens <hfstephens13@gmail.com>
2019-04-09 15:12:11 -0500

    creating plots for 0_1_4

--- matrix_plot/matrixMaps.py
+++ matrix_plot/matrixMaps.py
@@ -87,9 +87,7 @@ def makeSubplot(data, var, ax, row_num, col_num, ylabel, parallels, meridians, t
                            'pcldt', 'pscld', 'pdcld', 'wtrcld', 'icecld', 'ZSI']
                             #list of sequential variables to use for cmap
         if var in sequential_list:
-            # cmap = 'Blues_r'
-            cmap = plt.get_cmap('Blues_r')
-            cmap.set_over(color='k', alpha=1.)
+            cmap = 'Blues_r'
             norm = None
         else:
             cmap = 'seismic'
