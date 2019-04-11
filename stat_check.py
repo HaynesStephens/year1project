437583e3f60dfcfbe54badd28f29f0f1f1c400d5
HaynesStephens <hfstephens13@gmail.com>
2019-04-11 10:39:13 -0500

    ice growth

--- stat_check.py
+++ stat_check.py
@@ -33,11 +33,11 @@ def iceGrowth(filedir, filename1, filename2):
     #
     # shrink = z_change
     # shrink[shrink > 0] = 0
-    fig, (ax1, ax2) = plt.subplots(2)
+    fig, (ax1, ax2) = plt.subplots(2, 1)
     ax1.imshow(z_change, cmap='seismic', vmin = -1, vmax = 1)
-    ax1.colorbar()
+    plt.colorbar(cax = ax1, ax=ax1)
     ax2.imshow(z_change, cmap='seismic', vmin = np.max(np.abs(z_change))*-1, vmax = np.max(np.abs(z_change)))
-    ax2.colorbar()
+    plt.colorbar(cax=ax2, ax=ax2)
     plt.title('Ice Thickness Growth [m]')
     plt.tight_layout()
     plt.show()
