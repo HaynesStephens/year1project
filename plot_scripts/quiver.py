2da4aedfc5ae8598dd474a85ab8f171a37d55947
HaynesStephens <hfstephens13@gmail.com>
2019-04-13 13:39:35 -0500

    quiver plot

--- plot_scripts/quiver.py
+++ plot_scripts/quiver.py
@@ -111,10 +111,10 @@ def quiverPlot(col, ax):
 
 fig, ax = plt.subplots(figsize = (10,7))
 
-quiverPlot(col_0, ax)
+quiverPlot(col_1, ax)
 
 fig.tight_layout(w_pad = 2.25)
-file_name = 'plots/quiver_aqua'
+file_name = 'plots/quiver_1p'
 # plt.savefig(file_name+'.svg')
 plt.savefig(file_name+'.pdf')
 plt.show()
