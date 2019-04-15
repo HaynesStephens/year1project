d56d6176fd7c39328d17d3d5e675b5925b0e0b55
HaynesStephens <hfstephens13@gmail.com>
2019-04-15 14:48:23 -0500

    new quivs

--- plot_scripts/quiver.py
+++ plot_scripts/quiver.py
@@ -78,8 +78,6 @@ def quiverPlot(col, ax, tit_ad):
     u = avgDataFiles3D(filedir, 'u')
     v = avgDataFiles3D(filedir, 'v')
     uv_mag = np.sqrt((u*u) + (v*v))
-    u = u/100
-    v = v/100
 
     if title == 'Dynamic (5L), Aquaplanet':
         u      = np.roll(u,      (u.shape[1])//2, axis=1)
