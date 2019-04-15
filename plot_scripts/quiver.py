a3e2d70c240b3b9c00b4632ed47b2518ef060e16
HaynesStephens <hfstephens13@gmail.com>
2019-04-15 14:31:01 -0500

    new quivs

--- plot_scripts/quiver.py
+++ plot_scripts/quiver.py
@@ -117,7 +117,7 @@ fig, ax = plt.subplots(figsize = (10,7))
 quiverPlot(col_39, ax, 'Surface Ocean Velocity')
 
 fig.tight_layout(w_pad = 2.25)
-file_name = 'plots/quiver_39p'
+file_name = 'plots/quiver_39p_o'
 # plt.savefig(file_name+'.svg')
 plt.savefig(file_name+'.pdf')
 plt.show()
