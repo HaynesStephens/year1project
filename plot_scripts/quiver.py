0f398f4747838cc93cb3cca2a7b66375bcceee32
HaynesStephens <hfstephens13@gmail.com>
2019-04-17 16:21:53 -0500

    new quivs

--- plot_scripts/quiver.py
+++ plot_scripts/quiver.py
@@ -101,8 +101,8 @@ def quiverPlot(col, ax, tit_ad, num_files = 10, filetype='oijl', unit_conv=0.1, 
     cs = m.contourf(x, y, uv_mag, ax=ax)
     m.ax.tick_params(labelsize=2)
     m.colorbar(mappable=cs, ax=ax, label='m/s')
-    q = m.quiver(x, y, u, v, ax=ax, scale_units='width', scale=300,
-                 pivot='middle', width=0.001, headwidth=5, headlength=5)
+    q = m.quiver(x, y, u, v, ax=ax, scale_units='width', scale=200,
+                 pivot='middle', width=0.001, headwidth=7, headlength=5)
     ax.quiverkey(q, X=0.93, Y=1.02, U=1, label = '1 m/s', labelpos = 'E')
 
     if title != 'Dynamic (5L), Aquaplanet':
