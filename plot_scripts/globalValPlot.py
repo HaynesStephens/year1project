15f61d4500b7f443a8be545c74543d59d74d8306
HaynesStephens <hfstephens13@gmail.com>
2019-05-01 16:19:28 -0500

    lewis comp plots

--- plot_scripts/globalValPlot.py
+++ plot_scripts/globalValPlot.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from glob import glob
 from files_n_vars import *
+from lat_lon_grid import *
 
 
 def avgDataFilesGlobal(filedir, var, num_files, filetype, unit_conv, depth, side):
