ba0449083dbd7668006ea1f933ffdd83b2e87589
HaynesStephens <hfstephens13@gmail.com>
2019-05-10 16:39:23 -0500

    new plot

--- plot_scripts/files_n_vars.py
+++ plot_scripts/files_n_vars.py
@@ -13,31 +13,31 @@ filedir39=filebase+'pc_proxcenb_ssc5L_TL_39p'
 
 # COLUMNS REPRESENT DIFFERENT RUNS
 col_0 = {'filedir':filedir0,   'parallels':[],        'SA':0,
-        'meridians':[],        'title':'Dynamic (5L), Aquaplanet'}
+        'meridians':[],        'title':'Aqua'}
 
 col_1 = {'filedir':filedir1,   'parallels':[-12, 12], 'SA':1,
-        'meridians':[-15, 15], 'title':'Dynamic (5L), 1% SS Cont'}
+        'meridians':[-15, 15], 'title':'1% Land'}
 
 col_4 = {'filedir':filedir4,   'parallels':[-16, 16], 'SA':4,
-        'meridians':[-30, 30], 'title':'Dynamic (5L), 4% SS Cont'}
+        'meridians':[-30, 30], 'title':'4% Land'}
 
 col_6 = {'filedir':filedir6,   'parallels':[-20, 20], 'SA':6,
-        'meridians':[-35, 35], 'title':'Dynamic (5L), 6% SS Cont'}
+        'meridians':[-35, 35], 'title':'6% Land'}
 
 col_11 = {'filedir':filedir11, 'parallels':[-24, 24], 'SA':11,
-        'meridians':[-50, 50], 'title':'Dynamic (5L), 11% SS Cont'}
+        'meridians':[-50, 50], 'title':'11% Land'}
 
 col_22 = {'filedir':filedir22, 'parallels':[-36, 36], 'SA':22,
-        'meridians':[-70, 70], 'title':'Dynamic (5L), 22% SS Cont'}
+        'meridians':[-70, 70], 'title':'22% Land'}
 
 col_26 = {'filedir':filedir26, 'parallels':[-40, 40], 'SA':26,
-        'meridians':[-75, 75], 'title':'Dynamic (5L), 26% SS Cont'}
+        'meridians':[-75, 75], 'title':'26% Land'}
 
 col_34 = {'filedir':filedir34, 'parallels':[-44, 44], 'SA':34,
-        'meridians':[-90, 90], 'title':'Dynamic (5L), 34% SS Cont'}
+        'meridians':[-90, 90], 'title':'34% Land'}
 
 col_39 = {'filedir':filedir39, 'parallels':[-48, 48], 'SA':39,
-        'meridians':[-95, 95], 'title':'Dynamic (5L), 39% SS Cont'}
+        'meridians':[-95, 95], 'title':'39% Land'}
 
 
 # ROWS REPRESENT DIFFERENT VARIABLES
