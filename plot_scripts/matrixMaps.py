81ff53bd538171a3e7d4e356999be10ea860d425
HaynesStephens <hfstephens13@gmail.com>
2019-05-10 13:44:17 -0500

    new plot

--- plot_scripts/matrixMaps.py
+++ plot_scripts/matrixMaps.py
@@ -57,7 +57,7 @@ def makeSubplot(data, var, max_val, ax, row_num, col_num, ylabel, parallels, mer
     plt.gca().patch.set_color('.25')
     cs = m.contourf(x, y, data, levels, ax=ax, cmap=cmap, norm=norm)
     m.ax.tick_params(labelsize=2)
-    m.colorbar(mappable=cs, ax=ax, boundaries=np.linspace(0, max_val, levels))
+    m.colorbar(mappable=cs, ax=ax, boundaries = [0,20,40,60,80,100])
 
 
     def ContLines(m, ax, var, x, y, data):
