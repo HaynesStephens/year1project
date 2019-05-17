ccf6c517431891ce00279e60bded38c3b93b8b1b
HaynesStephens <hfstephens13@gmail.com>
2019-05-17 15:38:34 -0500

    new plot

--- plot_scripts/quiverPlot.py
+++ plot_scripts/quiverPlot.py
@@ -85,9 +85,9 @@ def quiverPlot():
     fig, axes = plt.subplots(2,1, figsize = (10,7))
     col = col_4
     ax0 = axes[0]
-    quiverSubPlot(col=col, ax=ax0, tit_ad='Air Surface Velocity', filetype='aij', unit_conv=1, depth=None)
+    quiverSubPlot(col=col, ax=ax0, tit_ad='Air Surface Velocity', filetype='aijpc', unit_conv=1, depth=None)
     ax1 = axes[1]
-    quiverSubPlot(col=col, ax=ax1, tit_ad='Ocean Surface Velocity', filetype='oijl', unit_conv=0.1, depth=1)
+    quiverSubPlot(col=col, ax=ax1, tit_ad='Ocean Surface Velocity', filetype='oijlpc', unit_conv=0.1, depth=1)
 
     fig.tight_layout(w_pad = 2.25)
     file_name = 'plots/quiver_4p'
