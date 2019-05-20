29072d4411e2bec17ebeda7cf9012c39185d4fd8
HaynesStephens <hfstephens13@gmail.com>
2019-05-20 12:03:08 -0500

    new plot

--- plot_scripts/globalValPlot.py
+++ plot_scripts/globalValPlot.py
@@ -75,10 +75,10 @@ def globalValPlot():
     row = row_tsurf
     fig, ax = plt.subplots()
 
-    makeSubplot(col_list, ax, row, filetype='aijpc', side='Night Side')
+    makeSubplot(col_list, ax, row, filetype='aijpc', side='Sub-stellar')
 
     fig.tight_layout(w_pad = 2.25)
-    file_name = 'plots/side_night_tsurf'
+    file_name = 'plots/side_substel_tsurf'
     # plt.savefig(file_name+'.svg')
     plt.savefig(file_name+'.pdf')
     plt.show()
