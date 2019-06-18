c6651e6cff5386730d10b1d26616361c0ad38529
HaynesStephens <hfstephens13@gmail.com>
2019-06-18 16:08:15 -0500

    row matrix test

--- plot_scripts/matrixMaps.py
+++ plot_scripts/matrixMaps.py
@@ -23,7 +23,7 @@ def avgDataFiles(filedir, var, num_files = 10):
     return arr_avg
 
 
-def makeSubplot(data, var, cbar_data, axes, row_num, col_num, ylabel, parallels, meridians, title, plot_cbar=False):
+def makeSubplot(data, var, axes, row_num, col_num, ylabel, parallels, meridians, title):
     ax = axes[row_num, col_num]
     m = Basemap(ax = ax)
 
@@ -31,9 +31,8 @@ def makeSubplot(data, var, cbar_data, axes, row_num, col_num, ylabel, parallels,
     nx=data.shape[1]
     lons, lats = m.makegrid(nx, ny)
     x, y = m(lons, lats)
-
-    min_val = np.min(cbar_data)
-    max_val = np.max(cbar_data)
+    min_val = np.min(data)
+    max_val = np.max(data)
 
     def make_cmap(var):
         sequential_list = ['frac_land', 'pscld', 'pdcld', 'snowicefr', 'lwp',
@@ -51,15 +50,10 @@ def makeSubplot(data, var, cbar_data, axes, row_num, col_num, ylabel, parallels,
 
     cmap, norm, levels = make_cmap(var)
 
-    if plot_cbar:
-        #Create colorbar to be used and then wipe axis so cbar_data isn't actually plotted
-        cs_cbar = m.contourf(x, y, cbar_data, levels, ax=ax, cmap=cmap, norm=norm)
-        ax.clear()
-
     plt.gca().patch.set_color('.25')
     cs = m.contourf(x, y, data, levels, ax=ax, cmap=cmap, norm=norm)
     m.ax.tick_params(labelsize=2)
-    # m.colorbar(mappable=cs, ax=ax)
+    m.colorbar(mappable=cs, ax=ax)
 
     # draw parallels and meridians.
     m.drawparallels([-60, -30, 0, 30, 60], labels=[1,0,0,0], ax = ax,
@@ -88,53 +82,23 @@ def makeSubplot(data, var, cbar_data, axes, row_num, col_num, ylabel, parallels,
     if col_num==0:
         ax.set_ylabel(ylabel, fontsize=10, labelpad = 60, rotation=0, verticalalignment ='center')
 
-    if plot_cbar:
-        #Plot the colorbar on the final plot of the row
-        plt.colorbar(mappable=cs_cbar, cax=axes[row_num, col_num+1])
-
-def getDataAndMaxVal(col_list, var):
-    """
-    Used to get data for a given row and the max value in order to have uniform colorbars across a row.
-    :param col_list:
-    :param var:
-    :return:
-    """
-    data_list = []
-    max_val = 0
-    min_val = 0
-    for col in col_list:
-        filedir = col['filedir']
-        data = avgDataFiles(filedir, var, num_files=10)
-        data_list.append(data)
-        min_val = min(min_val, np.min(data))
-        max_val = max(max_val, np.max(data))
-    cbar_data = np.ones(shape = data.shape) * max_val # creates fake data w/ same shape to use for colorbar
-    cbar_data[0, 0] = min_val # sets one point in fake data to min_val so fake data has the entire spread
-    return data_list, cbar_data
-
 
 def matrixMaps():
-    # fig, axes = plt.subplots(len(row_list), len(col_list) + 1,
-    #                          figsize = (10,5),
-    #                          gridspec_kw={'width_ratios': [1]*len(col_list) + [1/len(col_list)]})
-    fig, axes = plt.subplots(len(row_list), len(col_list) + 1, figsize = (10,5))
+    fig, axes = plt.subplots(len(row_list), len(col_list) + 1,
+                             figsize = (10,5),
+                             gridspec_kw={'width_ratios': [1]*len(col_list) + [1/len(col_list)]})
 
     for row_num in range(len(row_list)):
         row = row_list[row_num]
         var = row['var']
-        data_list, cbar_data = getDataAndMaxVal(col_list, var)
-        print("MIN VAL: {0}, MAX VAL: {1}".format(np.min(cbar_data), np.max(cbar_data)))
         for col_num in range(len(col_list)):
             col = col_list[col_num]
             print(col_num, row_num)
-            data = data_list[col_num]
-            if col_num == len(col_list) - 1:
-                plot_cbar = True
-            else:
-                plot_cbar = False
-            makeSubplot(data=data, var=var, cbar_data=cbar_data, axes=axes,
+            filedir=col['filedir']
+            data = avgDataFiles(filedir, var)
+            makeSubplot(data=data, var=var, axes=axes,
                         row_num=row_num, col_num=col_num, ylabel=row['ylabel'], parallels=col['parallels'],
-                        meridians=col['meridians'], title=col['title'], plot_cbar=plot_cbar)
+                        meridians=col['meridians'], title=col['title'])
 
     fig.tight_layout(w_pad = 2.25)
     file_name = 'plots/row_matrix_test'
