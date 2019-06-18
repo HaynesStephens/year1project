d400270ef231d4ea726d9b6d6f0e0a5bfa4c52fd
HaynesStephens <hfstephens13@gmail.com>
2019-06-18 14:27:11 -0500

    row plots for committee mtg

--- plot_scripts/matrixMaps.py
+++ plot_scripts/matrixMaps.py
@@ -117,7 +117,7 @@ def matrixMaps():
     # fig, axes = plt.subplots(len(row_list), len(col_list) + 1,
     #                          figsize = (10,5),
     #                          gridspec_kw={'width_ratios': [1]*len(col_list) + [1/len(col_list)]})
-    fig = plt.figure(figsize = (10,5))
+    fig, axes = plt.subplots(len(row_list), len(col_list) + 1, figsize = (10,5))
 
     for row_num in range(len(row_list)):
         row = row_list[row_num]
