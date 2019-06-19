321f05c09d694389521e75d4e1e13f4cf352c158
HaynesStephens <hfstephens13@gmail.com>
2019-06-19 14:07:01 -0500

    getting slice plot for density structure

--- plot_scripts/slicePlot.py
+++ plot_scripts/slicePlot.py
@@ -72,10 +72,10 @@ def sliceSubplot(data, slice_dim, slice_coord, axes, row_num, col_num,
         ax.set_xticklabels(lon_grid)
     elif slice_dim == 'lon':
         ax.set_xticklabels(lat_grid)
-    ax.set_yticklabels(z_grid)
 
     if row_num == 0:
         ax.set_title(title, fontsize=10)
+        ax.set_yticklabels(z_grid)
 
     if col_num == 0:
         ax.set_ylabel(ylabel, fontsize=10, labelpad = 60, rotation=0, verticalalignment ='center')
