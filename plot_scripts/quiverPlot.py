f52b05facf73db9dd68f908717f906e88059274d
HaynesStephens <hfstephens13@gmail.com>
2019-06-21 10:16:26 -0500

    generating new quiver plots by depth

--- plot_scripts/quiverPlot.py
+++ plot_scripts/quiverPlot.py
@@ -87,10 +87,10 @@ def quiverPlot():
     ax0 = axes[0]
     quiverSubPlot(col=col, ax=ax0, tit_ad='Air Surface Velocity', filetype='aijpc', unit_conv=1, depth=None)
     ax1 = axes[1]
-    quiverSubPlot(col=col, ax=ax1, tit_ad='Ocean Surface Velocity', filetype='oijlpc', unit_conv=0.1, depth=0)
+    quiverSubPlot(col=col, ax=ax1, tit_ad='Ocean Surface Velocity', filetype='oijlpc', unit_conv=0.1, depth=1)
 
     fig.tight_layout(w_pad = 2.25)
-    file_name = 'plots/quiver_0p'
+    file_name = 'plots/quiver_0_1p'
     # plt.savefig(file_name+'.svg')
     plt.savefig(file_name+'.pdf')
     plt.show()
