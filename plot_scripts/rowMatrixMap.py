e729b8169a0d2b1baa0abddd3675cf51a344ed98
HaynesStephens <hfstephens13@gmail.com>
2019-06-21 10:55:16 -0500

    generating new quiver plots by depth

--- plot_scripts/rowMatrixMap.py
+++ plot_scripts/rowMatrixMap.py
@@ -148,7 +148,7 @@ def rowMatrixMap(row_list, col_list, filetype, depth = None):
                         meridians=col['meridians'], title=col['title'], plot_cbar=plot_cbar)
 
     # fig.tight_layout(w_pad = 2.25)
-    file_name = 'plots/row_matrix_o_w'
+    file_name = 'plots/row_matrix_o_w_0'
     # plt.savefig(file_name+'.svg')
     plt.savefig(file_name+'.pdf')
     plt.show()
