e83a1106cd1b61815196d60254b520d1ba20c198
HaynesStephens <hfstephens13@gmail.com>
2019-06-21 10:11:42 -0500

    getting slice plot for density structure

--- plot_scripts/quiverPlot.py
+++ plot_scripts/quiverPlot.py
@@ -83,14 +83,14 @@ def quiverSubPlot(col, ax, tit_ad, filetype, unit_conv, depth, num_files = 10):
 
 def quiverPlot():
     fig, axes = plt.subplots(2,1, figsize = (10,7))
-    col = col_0
+    col = col_1
     ax0 = axes[0]
     quiverSubPlot(col=col, ax=ax0, tit_ad='Air Surface Velocity', filetype='aijpc', unit_conv=1, depth=None)
     ax1 = axes[1]
     quiverSubPlot(col=col, ax=ax1, tit_ad='Ocean Surface Velocity', filetype='oijlpc', unit_conv=0.1, depth=0)
 
     fig.tight_layout(w_pad = 2.25)
-    file_name = 'plots/quiver_0p'
+    file_name = 'plots/quiver_1p'
     # plt.savefig(file_name+'.svg')
     plt.savefig(file_name+'.pdf')
     plt.show()
