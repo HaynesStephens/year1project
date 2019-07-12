b6ee0b56986d22276fa776851bfbee29f3796bbe
HaynesStephens <hfstephens13@gmail.com>
2019-07-12 08:19:36 -0700

    zonal average wind

--- rocke_ss.py
+++ rocke_ss.py
@@ -13,12 +13,12 @@ import pandas as pd
 from glob import glob
 
 ## ***SPECIFY EXPERIMENT & ITS LOCATION ON MIDWAY***
-runid = 'pc_proxcenb_ssc5L_TL_4p'
+runid = 'pc_proxcenb_ssc5L_TL_6p'
 rundirectory = '/project2/abbot/haynes/ROCKE3D_output/' + runid
 
 endyear_dict = {'0p':3500, '1p':4140, '4p':3549, '6p':4179,
                 '11p':4379, '22p':3729, '26p':3649, '34p':2769, '39p':2649}
-endyear = endyear_dict['4p']
+endyear = endyear_dict['6p']
 startyear = endyear - 99
 # startyear = ####
 
