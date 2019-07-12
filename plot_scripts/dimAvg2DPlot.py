47c1f80b355d51686c6d8d6bf43d38214fd82273
HaynesStephens <hfstephens13@gmail.com>
2019-07-12 10:16:02 -0700

    zonal average wind

--- plot_scripts/dimAvg2DPlot.py
+++ plot_scripts/dimAvg2DPlot.py
@@ -60,9 +60,9 @@ def makeSubplot(data, grid, row, col, coord, seq_or_div, rot_origin):
 
     extent = (-90, 90, row['z'][0], row['z'][-1])
     if rot_origin:
-        im = ax.contourf(data, cmap=cmap, norm=norm, origin ='lower')#, extent=extent, aspect = 0.1)
+        im = ax.contourf(data, cmap=cmap, norm=norm, levels=levels, origin ='lower')#, extent=extent, aspect = 0.1)
     else:
-        im = ax.contourf(data, cmap=cmap, norm=norm, origin='upper')#, extent=extent, aspect = 0.1)
+        im = ax.contourf(data, cmap=cmap, norm=norm, levels=levels, origin='upper')#, extent=extent, aspect = 0.1)
 
     def plotContLatLine(ax, col):
         ax.plot(col['parallels'], [-0.5, -0.5], c='k')
