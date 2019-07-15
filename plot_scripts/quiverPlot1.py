4cd184d217527e7756923540cb5774f7004bf46a
HaynesStephens <hfstephens13@gmail.com>
2019-07-15 12:25:37 -0700

    new quiver attempt

--- plot_scripts/quiverPlot1.py
+++ plot_scripts/quiverPlot1.py
@@ -53,7 +53,7 @@ def makeSubplot(grid, row_u, u, v, row_contour, contour_data, col, title, seq_or
             uv_mag = np.sqrt((u * u) + (v * v))
             q = ax.quiver(x_u, y_u, u/uv_mag, v/uv_mag)
         else:
-            q = ax.quiver(x_u, y_u, u, v, scale_units='width', scale=500,
+            q = ax.quiver(x_u, y_u, u, v, scale_units='width', scale=1000,
                  pivot='middle', width=0.001, headwidth=5, headlength=5)
             U = 10
             key_label = '{0} m/s'.format(U)
