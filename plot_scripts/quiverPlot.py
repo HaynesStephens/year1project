2291b389d82e66a3e946b587fdb04c6824df637b
HaynesStephens <hfstephens13@gmail.com>
2019-07-16 13:43:04 -0700

    adding height to quiver plots

--- plot_scripts/quiverPlot.py
+++ plot_scripts/quiverPlot.py
@@ -156,8 +156,8 @@ def getTitle(row_u, row_contour, col, depth, filetype_uv):
         if 'o' in filetype_uv:
             z_arr = row_u['z']
         elif 'a' in filetype_uv:
-            z_arr = getHeightFile(col['filedir'], filetype_uv) // 1
-        title_depth = ', {0.0f} m'.format(str(z_arr[depth]))
+            z_arr = getHeightFile(col['filedir'], filetype_uv)
+        title_depth = ', {0} m'.format(int(str(z_arr[depth])))
     title = '{0}{1}, {2}'.format(col['title'], title_depth, title_row)
     return title
 
