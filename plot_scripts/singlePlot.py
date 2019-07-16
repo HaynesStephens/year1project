e1ccf18cace32361fb78ff52fecb0413bfa62b0d
HaynesStephens <hfstephens13@gmail.com>
2019-07-16 07:59:06 -0700

    planetary albedo

--- plot_scripts/singlePlot.py
+++ plot_scripts/singlePlot.py
@@ -41,8 +41,8 @@ def makeSubplot(grid, data, row, col, title, seq_or_div):
     # max_val = np.max(np.abs(data))
 
     def make_cmap(seq_or_div):
-        min_val = 100
-        max_val = 0
+        min_val = 0
+        max_val = 100
         levels = np.linspace(min_val, max_val, 50)
         if seq_or_div == 'seq':
             cmap = cm.Blues_r
