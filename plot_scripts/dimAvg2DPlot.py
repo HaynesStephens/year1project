cde632ff39f75c127cf446946168e1c0f42e59ec
HaynesStephens <hfstephens13@gmail.com>
2019-07-16 11:25:09 -0700

    putting height on zonal winds

--- plot_scripts/dimAvg2DPlot.py
+++ plot_scripts/dimAvg2DPlot.py
@@ -96,7 +96,7 @@ def makeSubplot(data, filetype, grid, row, col, coord, seq_or_div):
             ax.plot(col['parallels'], [y[0], y[0]], c='k', linewidth=3)
 
     im = ax.contourf(x, y, data, levels, cmap=cmap, norm=norm)
-    ax.set_aspect(0.1)
+    ax.set_aspect(0.01)
 
     cbar = grid.cbar_axes[0].colorbar(im)
     cbar.set_label_text(row['units'])
