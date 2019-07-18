d94ae0fcc2b572daef400d90075e90362e099381
HaynesStephens <hfstephens13@gmail.com>
2019-07-18 08:14:56 -0700

    cloudiness plots

--- plot_scripts/singlePlot.py
+++ plot_scripts/singlePlot.py
@@ -44,7 +44,7 @@ def makeSubplot(grid, data, row, col, title, seq_or_div):
     def make_cmap(seq_or_div):
         min_val = -150
         max_val = 150
-        levels = np.linspace(min_val, max_val, 31)
+        levels = np.linspace(min_val, max_val, 21)
         if seq_or_div == 'seq':
             cmap = cm.Blues
             norm = Normalize(vmin = min_val, vmax = max_val)
