6c35b89578dbf71a6f456e6f85127820a7efee6d
HaynesStephens <hfstephens13@gmail.com>
2019-07-18 14:52:14 -0700

    ocean quiver

--- plot_scripts/quiverPlot.py
+++ plot_scripts/quiverPlot.py
@@ -22,6 +22,8 @@ def avgDataFiles(filedir, filetype, var, unit_conv = 1, num_files=10):
         arr_avg = np.roll(arr_avg, (arr_avg.shape[2]) // 2, axis=2)
     if 'o' in filetype: #if it's ocean file, only take the top 5 levels
         arr_avg = arr_avg[:5, :, :]
+
+    print(arr_avg[0, -1, 0])
     return arr_avg
 
 
@@ -192,7 +194,6 @@ def quiverPlot(row_u, row_v, row_contour, col, filetype_uv, filetype_contour,
         contour_data = avgDataFiles(filedir, filetype_contour, var_contour)
         print("{0} MIN: {1}, {0} MAX: {2}".format(var_contour, np.min(contour_data), np.max(contour_data)))
         contour_data = depthOrVertAvg(contour_data, depth_contour)
-        print(contour_data[-1,:])
     else:
         contour_data = np.sqrt((u * u) + (v * v)) # Set contours as the horizontal velocity magnitudes
         print("{0} MIN: {1}, {0} MAX: {2}".format('Velocity', np.min(contour_data), np.max(contour_data)))
