18de541a3b8b1b0c5862400989043afc38cc0598
HaynesStephens <hfstephens13@gmail.com>
2019-07-18 11:40:57 -0700

    cloudiness plots

--- plot_scripts/singlePlot.py
+++ plot_scripts/singlePlot.py
@@ -165,7 +165,7 @@ def singlePlot(row, col, filetype, depth, seq_or_div):
     print('PLOT NAME:', file_name)
 
     # plt.savefig(file_name+'.svg')
-    # plt.savefig(file_name+'.pdf')
+    plt.savefig(file_name+'.pdf')
     # plt.show()
     print('Plot saved.')
 
