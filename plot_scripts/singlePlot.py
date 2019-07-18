52521aefdc076494323069862348518872625275
HaynesStephens <hfstephens13@gmail.com>
2019-07-18 10:33:36 -0700

    cloudiness plots

--- plot_scripts/singlePlot.py
+++ plot_scripts/singlePlot.py
@@ -173,7 +173,7 @@ def singlePlot(row, col, filetype, depth, seq_or_div):
 row = row_snowicefr
 col = col_39
 filetype = 'aijpc'
-seq_or_div = 'div'
+seq_or_div = 'seq'
 col_list = [col_0, col_1, col_4, col_6, col_11, col_22, col_26, col_34, col_39]
 
 
