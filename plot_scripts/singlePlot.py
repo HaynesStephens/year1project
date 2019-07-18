01fb40c15dfeb4599548f11a3d503c6a2f0626c4
HaynesStephens <hfstephens13@gmail.com>
2019-07-18 08:40:13 -0700

    cloudiness plots

--- plot_scripts/singlePlot.py
+++ plot_scripts/singlePlot.py
@@ -42,9 +42,9 @@ def makeSubplot(grid, data, row, col, title, seq_or_div):
     # max_val = np.max(np.abs(data))
 
     def make_cmap(seq_or_div):
-        min_val = -100
-        max_val = 100
-        levels = np.linspace(min_val, max_val, 11)
+        min_val = -600
+        max_val = 600
+        levels = np.linspace(min_val, max_val, 31)
         if seq_or_div == 'seq':
             cmap = cm.Blues_r
             norm = Normalize(vmin = min_val, vmax = max_val)
@@ -166,20 +166,20 @@ def singlePlot(row, col, filetype, depth, seq_or_div):
 
     # plt.savefig(file_name+'.svg')
     # plt.savefig(file_name+'.pdf')
-    # plt.show()
+    plt.show()
     print('Plot saved.')
 
 
 row = row_net_rad_planet
-# col = col_39
+col = col_39
 filetype = 'aijpc'
 seq_or_div = 'div'
 col_list = [col_0, col_1, col_4, col_6, col_11, col_22, col_26, col_34, col_39]
 
 
-# ############# SINGLE DEPTH PLOT #################
-# depth = None
-# singlePlot(row, col, filetype, depth, seq_or_div)
+############# SINGLE DEPTH PLOT #################
+depth = None
+singlePlot(row, col, filetype, depth, seq_or_div)
 
 # ############# ALL DEPTHS PLOT ###################
 # for depth_i in range(row['z'].size):
@@ -196,9 +196,9 @@ col_list = [col_0, col_1, col_4, col_6, col_11, col_22, col_26, col_34, col_39]
 #         singlePlot(row, col_i, filetype, depth_i, seq_or_div)
 #     singlePlot(row, col_i, filetype, 'vertAvg', seq_or_div)
 
-############# ALL CALLS OF SAME DEPTH ################
-depth = None
-for col_i in col_list:
-    singlePlot(row, col_i, filetype, depth, seq_or_div)
+# ############# ALL CALLS OF SAME DEPTH ################
+# depth = None
+# for col_i in col_list:
+#     singlePlot(row, col_i, filetype, depth, seq_or_div)
 
 
