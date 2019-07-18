6186bdc40a89b0c50977d6cb772887442d553ca9
HaynesStephens <hfstephens13@gmail.com>
2019-07-18 09:36:19 -0700

    cloudiness plots

--- plot_scripts/globalValPlot.py
+++ plot_scripts/globalValPlot.py
@@ -138,7 +138,7 @@ def globalValPlot(row, side):
 
 # row = {'var':'plan_alb_calc'}
 side_list = ['Global', 'Day Side', 'Night Side', 'Sub-stellar']
-row = row_pcldm
+row = row_pcldh
 
 for side_i in side_list:
     globalValPlot(row, side_i)
